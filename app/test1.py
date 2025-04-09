f7a33458591c3bffd9e6ee80ce82b910d4357396
RomanDiachenko <52920901+RomanDiachenko@users.noreply.github.com>
2025-04-09 15:46:54 +0300

    Update test1.py

--- app/test1.py
+++ app/test1.py
@@ -1,5 +1,12 @@
 import pytest
+import allure
 
-def test():
+@allure.feature("test")
+@allure.story("Test story")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.title("Test title")
+@allure.description("Test description")
+@allure.tag("test")
+def test_test():
     print ("hello. This is test allure report")
     assert True
